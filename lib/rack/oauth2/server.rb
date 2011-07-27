efddcfa11b6d0e40a8f45444949f9d6c2e64f092
Matthew Whittaker <mwhitt89@gmail.com>
2011-07-27 12:35:27 -0700

    changed unauthorized to return 403 instead of 401 so that it plays nicely with devise

--- lib/rack/oauth2/server.rb
+++ lib/rack/oauth2/server.rb
@@ -434,7 +434,7 @@ module Rack
       def unauthorized(request, error = nil)
         challenge = 'OAuth realm="%s"' % (options.realm || request.host)
         challenge << ', error="%s", error_description="%s"' % [error.code, error.message] if error
-        return [401, { "WWW-Authenticate"=>challenge }, [error && error.message || ""]]
+        return [403, { "WWW-Authenticate"=>challenge }, [error && error.message || ""]]
       end
 
       # Wraps Rack::Request to expose Basic and OAuth authentication
